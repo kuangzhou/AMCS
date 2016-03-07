3e01a57ac4b9b996217bb4290c8baba45feec6e0
Yijing Li <liyijing024@yahoo.com>
2016-03-07 23:17:03 +0800

    Update FAK.m

--- FAK/FAK.m
+++ FAK/FAK.m
@@ -2,7 +2,7 @@ function  FAK(train_data,lab_tr,test_data,lab_ts,cn)
 % Using FCBF as feature selection method, Adaboost as ensemble framework and KNN as base classifier
 addpath(genpath(pwd));
 gBest=FCBF(train_data,lab_tr);
-tsnum=length(lab_tr);
+tsnum=length(lab_ts);
 [FAhypo,AUCarea]=FCompareAUC(train_data,lab_tr,test_data,lab_ts,gBest);
 AUCarea
 acc=length(find(FAhypo'==lab_ts))/tsnum

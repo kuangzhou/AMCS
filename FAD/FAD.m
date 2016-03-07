ea99d6a20f13cbc56344aa917a8cc5d9586bfb2e
Yijing Li <liyijing024@yahoo.com>
2016-03-07 23:16:32 +0800

    Update FAD.m

--- FAD/FAD.m
+++ FAD/FAD.m
@@ -2,7 +2,7 @@ function  FAD(train_data,lab_tr,test_data,lab_ts,cn)
 % Using FCBF as feature selection method, Adaboost as ensemble framework and DGC as base classifier
 addpath(genpath(pwd));
 gBest=FCBF(train_data,lab_tr);
-tsnum=length(lab_tr);
+tsnum=length(lab_ts);
 
 [AUCarea,FAhypo]=FCompareAUC(train_data,lab_tr,test_data,lab_ts,gBest);
 AUCarea

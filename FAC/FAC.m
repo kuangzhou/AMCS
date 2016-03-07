8a81363554537bc35a7dc4b7ad29852c9961a0ef
Yijing Li <liyijing024@yahoo.com>
2016-03-07 23:16:05 +0800

    Update FAC.m

--- FAC/FAC.m
+++ FAC/FAC.m
@@ -2,7 +2,7 @@ function  FAC(train_data,lab_tr,test_data,lab_ts,cn)
 % Using FCBF as feature selection method, Adaboost as ensemble framework and SVM as base classifier
 addpath(genpath(pwd));
 gBest=FCBF(train_data,lab_tr);
-tsnum=length(lab_tr);
+tsnum=length(lab_ts);
 [AUCarea,FAhypo]=FCompareAUC(train_data,lab_tr,test_data,lab_ts,gBest);
 AUCarea
 Fcount=zeros(1,cn);

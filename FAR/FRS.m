7afa1d8d482173d743551741c690aac05ea42875
Yijing Li <liyijing024@yahoo.com>
2016-03-07 23:18:24 +0800

    Update FRS.m

--- FAR/FRS.m
+++ FAR/FRS.m
@@ -1,7 +1,7 @@
 function  FRS(train_data,lab_tr,test_data,lab_ts,cn)
 % Using FCBF as feature selection method, Adaboost as ensemble framework and RBF as base classifier
 addpath(genpath(pwd));
-gBest=FCBF(train_data,lab_tr);tsnum=length(lab_tr);
+gBest=FCBF(train_data,lab_tr);tsnum=length(lab_ts);
 
 [AUCarea,FAhypo]=FCompareAUC(train_data,lab_tr,test_data,lab_ts,gBest);
 AUCarea
